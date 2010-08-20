55f3f226a07518d5343801b47645400d9b0b4770
David M Peixotto <dmp@rice.edu>
2010-08-20 11:26:17 -0500

    Updated for integration with fibon

--- Hackage/HaLeX/Fibon/Instance.hs
+++ Hackage/HaLeX/Fibon/Instance.hs
@@ -1,4 +1,5 @@
-module Fibon.Benchmarks.Hackage.HaLeX (
+{-# OPTIONS_GHC -fno-warn-missing-signatures #-}
+module Fibon.Benchmarks.Hackage.HaLeX.Fibon.Instance (
   mkInstance
 )
 where
@@ -15,11 +16,10 @@ sharedConfig = BenchmarkInstance {
     , localPath      = "Hackage/HaLeX"
     , exeName        = "halex"
   }
-
-}
+flgCfg = flagConfig sharedConfig
 
 mkInstance :: InputSize -> BenchmarkInstance
-mkInstance Test = sharedConfig {flagConfig = flgCfg {runFlags = "vowles"}}
-mkInstance Ref  = sharedConfig {flagConfig = flgCfg {runFlags = "real"}}
-  where flgCfg = flagConfig sharedConfig
+mkInstance Test = sharedConfig {flagConfig = flgCfg {runFlags = ["vowles"]}}
+mkInstance Ref  = sharedConfig {flagConfig = flgCfg {runFlags = ["real"]}}
+
 
