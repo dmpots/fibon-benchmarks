3af629d4578d4b417f980ce7e386692b85b891ea
David M Peixotto <dmp@rice.edu>
2010-08-24 10:08:29 -0500

    Added cpsa benchmark

---
+++ Hackage/Cpsa/Fibon/Instance.hs
@@ -0,0 +1,27 @@
+{-# OPTIONS_GHC -fno-warn-missing-signatures #-}
+module Fibon.Benchmarks.Hackage.Cpsa.Fibon.Instance(
+  mkInstance
+)
+where
+import Fibon.BenchmarkInstance
+
+sharedConfig = BenchmarkInstance {
+    flagConfig = FlagConfig {
+        configureFlags = []
+      , buildFlags     = []
+      , runFlags       = []
+      }
+    , stdinInput     = Nothing
+    , output         = [(Stdout, Diff "cpsa.stdout.expected")]
+    , localPath      = "Hackage/Cpsa"
+    , exeName        = "cpsa"
+  }
+flgCfg = flagConfig sharedConfig
+
+mkInstance Test = sharedConfig {
+        flagConfig = flgCfg {runFlags = ["ns.scm"]}
+    }
+mkInstance Ref  = sharedConfig {
+        flagConfig = flgCfg {runFlags = ["nsl5.lisp"]}
+    }
+
