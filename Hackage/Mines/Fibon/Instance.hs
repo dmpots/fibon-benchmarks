54122717d9e0f350683abaaa47bbbd21caabb279
David M Peixotto <dmp@rice.edu>
2010-08-24 15:57:25 -0500

    Added mines benchmark

---
+++ Hackage/Mines/Fibon/Instance.hs
@@ -0,0 +1,28 @@
+{-# OPTIONS_GHC -fno-warn-missing-signatures #-}
+module Fibon.Benchmarks.Hackage.Mines.Fibon.Instance(
+  mkInstance
+)
+where
+import Fibon.BenchmarkInstance
+
+sharedConfig = BenchmarkInstance {
+    flagConfig = FlagConfig {
+        configureFlags = []
+      , buildFlags     = []
+      , runFlags       = []
+      }
+    , stdinInput     = Nothing
+    , output         = [(Stdout, Diff "mines.stdout.expected"),
+                        (OutputFile "mines.svg", Diff "gen1.svg.expected")]
+    , localPath      = "Hackage/Mines"
+    , exeName        = "mines"
+  }
+flgCfg = flagConfig sharedConfig
+
+mkInstance Test = sharedConfig {
+        flagConfig = flgCfg {runFlags = ["1"]}
+    }
+mkInstance Ref  = sharedConfig {
+        flagConfig = flgCfg {runFlags = ["10"]}
+    }
+
