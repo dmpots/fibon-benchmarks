a1bcdcd56f4fe0a7ca807d66993931a276bb7fed
David M Peixotto <dmp@rice.edu>
2010-08-24 14:53:11 -0500

    Added xsact benchmark

---
+++ Hackage/Xsact/Fibon/Instance.hs
@@ -0,0 +1,27 @@
+{-# OPTIONS_GHC -fno-warn-missing-signatures #-}
+module Fibon.Benchmarks.Hackage.Xsact.Fibon.Instance(
+  mkInstance
+)
+where
+import Fibon.BenchmarkInstance
+
+sharedConfig = BenchmarkInstance {
+    flagConfig = FlagConfig {
+        configureFlags = []
+      , buildFlags     = []
+      , runFlags       = []
+      }
+    , stdinInput     = Nothing
+    , output         = [(Stdout, Diff "xsact.stdout.expected")]
+    , localPath      = "Hackage/Xsact"
+    , exeName        = "xsact"
+  }
+flgCfg = flagConfig sharedConfig
+
+mkInstance Test = sharedConfig {
+        flagConfig = flgCfg {runFlags = ["-i", "alu.n"]}
+    }
+mkInstance Ref  = sharedConfig {
+        flagConfig = flgCfg {runFlags = ["-i", "pdbnt"]}
+    }
+
