da6f93b2f80db14d1833e6edde44259899b2c4ae
David M Peixotto <dmp@rice.edu>
2010-08-27 12:06:23 -0500

    Added fst benchmark

---
+++ Hackage/Fst/Fibon/Instance.hs
@@ -0,0 +1,27 @@
+{-# OPTIONS_GHC -fno-warn-missing-signatures #-}
+module Fibon.Benchmarks.Hackage.Fst.Fibon.Instance(
+  mkInstance
+)
+where
+import Fibon.BenchmarkInstance
+
+sharedConfig = BenchmarkInstance {
+    flagConfig = FlagConfig {
+        configureFlags = []
+      , buildFlags     = []
+      , runFlags       = ["-u", "soda.fst"]
+      }
+    , stdinInput     = Just "PLONK"
+    , output         = [(Stdout, Diff "fst.stdout.expected")]
+    , localPath      = "Hackage/Fst"
+    , exeName        = "fst"
+  }
+flgCfg = flagConfig sharedConfig
+
+mkInstance Test = sharedConfig {
+        flagConfig = flgCfg
+    }
+mkInstance Ref  = sharedConfig {
+        flagConfig = flgCfg
+    }
+
