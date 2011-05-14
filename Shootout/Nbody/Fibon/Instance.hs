9d1154d9d8a2fb7d01a00a43ec248d0896ebe3b3
David M Peixotto <dmp@rice.edu>
2011-05-14 09:02:04 -0500

    Increased size of Nbody Ref inputs

--- Shootout/Nbody/Fibon/Instance.hs
+++ Shootout/Nbody/Fibon/Instance.hs
@@ -21,7 +21,10 @@ flgCfg = flagConfig sharedConfig
 mkInstance Test = sharedConfig {
         flagConfig = flgCfg {runFlags = ["1000"]}
     }
-mkInstance Ref  = sharedConfig {
+mkInstance Train = sharedConfig {
         flagConfig = flgCfg {runFlags = ["10000000"]}
+    }
+mkInstance Ref  = sharedConfig {
+        flagConfig = flgCfg {runFlags = ["1600000000"]}
     }
 
